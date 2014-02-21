9e886372c91e51ba2c0bf3c0e66645e9fc70e039
Rodrigo <email@rodrigodossantos.ws>
2014-02-22 09:54:37 +1100

    Add use by validation

---
+++ app/src/Fridge/Items.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Fridge;
+
+class Items
+{
+
+    public function getItemFromCsvFile()
+    {
+        $csvFile = __DIR__ . '/../../../data/fridge.csv';
+
+        $file = new \SplFileObject($csvFile);
+
+        $file->setFlags(\SplFileObject::READ_CSV);
+        $file->setCsvControl(',', '"', '\\');
+
+        $items = [];
+
+        foreach ($file as $item) {
+
+            list ($itemName, $amount, $unit, $useBy) = $item;
+
+            $items[] = [
+                'item'   => $itemName,
+                'amount' => $amount,
+                'unit'   => $unit,
+                'useBy'  => $useBy
+            ];
+        }
+
+        return $items;
+    }
+
+    public function hasExpired($item)
+    {
+
+        return strtotime(str_replace('/', '-', $item['useBy'])) < time();
+    }
+
+}
