2231896dc702170e5e980631142b60a0524b1ca3
Rodrigo <email@rodrigodossantos.ws>
2014-02-22 22:13:43 +1100

    Add documentation

--- app/src/Fridge/Items.php
+++ app/src/Fridge/Items.php
@@ -1,9 +1,36 @@
 <?php
-
+/**
+ * Items in the fridge
+ *
+ * PHP version 5.5.8
+ *
+ * @category RecipeFinder
+ * @package  Fridge
+ * @author   Rodrigo dos Santos <rodrigo@dossantos.com.au>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     https://github.com/rosantoz
+ */
 namespace Fridge;
 
+/**
+ * Items in the fridge
+ *
+ * @category RecipeFinder
+ * @package  Fridge
+ * @author   Rodrigo dos Santos <email@rodrigodossantos.ws>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     https://github.com/rosantoz
+ */
 class Items
 {
+    /**
+     * Parses a csv file containing the items in the fridge
+     * and convert them into an array of items
+     *
+     * @param string $csvFile Path to CSV file
+     *
+     * @return array
+     */
     public function getItemFromCsvFile($csvFile)
     {
         $file = new \SplFileObject($csvFile);
--- app/src/Recipe/Finder.php
+++ app/src/Recipe/Finder.php
@@ -1,18 +1,49 @@
 <?php
-
+/**
+ * Recipe Finder
+ *
+ * PHP version 5.5.8
+ *
+ * @category RecipeFinder
+ * @package  Recipe
+ * @author   Rodrigo dos Santos <rodrigo@dossantos.com.au>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     https://github.com/rosantoz
+ */
 namespace Recipe;
 
+/**
+ * Recipe Finder. Checks the recipes and items in the fridge an
+ * returns a suggestion of what to cook tonight
+ *
+ * @category RecipeFinder
+ * @package  Recipe
+ * @author   Rodrigo dos Santos <email@rodrigodossantos.ws>
+ * @license  http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @link     https://github.com/rosantoz
+ */
 class Finder
 {
     protected $items;
     protected $recipes;
 
+    /**
+     * Constructor
+     *
+     * @param array $items   Items in the fridge
+     * @param array $recipes Recipes
+     */
     public function __construct(array $items, array $recipes)
     {
         $this->items   = $items;
         $this->recipes = $recipes;
     }
 
+    /**
+     * Returns a list of what to cook
+     *
+     * @return array
+     */
     public function find()
     {
         $matchingItems = [];
@@ -41,11 +72,27 @@ class Finder
         return $matchingItems;
     }
 
+    /**
+     * Returns the ingredients of a recipe
+     *
+     * @param array $recipe Recipe Array
+     *
+     * @return array|null
+     */
     public function getIngredients($recipe)
     {
         return isset($recipe['ingredients']) ? $recipe['ingredients'] : null;
     }
 
+    /**
+     * Checks if the recipe ingredient is in the fridge,
+     * has the required amount and unit
+     * and has not passed its use-by date.
+     *
+     * @param string $ingredient Ingredient Name
+     *
+     * @return bool
+     */
     public function isInTheFridge($ingredient)
     {
         foreach ($this->items as $item) {
@@ -53,7 +100,7 @@ class Finder
             if (($item['item'] === $ingredient['item'])
                 && ($item['amount'] >= $ingredient['amount'])
                 && ($item['unit'] === $ingredient['unit'])
-                && ($this->hasExpired($item['useBy']))
+                && (!$this->hasExpired($item['useBy']))
             ) {
                 return true;
             }
@@ -62,8 +109,15 @@ class Finder
         return false;
     }
 
+    /**
+     * Checks if a given date string is in the past
+     *
+     * @param string $useBy Date (dd/mm/YYYY)
+     *
+     * @return bool
+     */
     protected function hasExpired($useBy)
     {
-        return strtotime(str_replace('/', '-', $useBy)) > time();
+        return strtotime(str_replace('/', '-', $useBy)) < time();
     }
 }
