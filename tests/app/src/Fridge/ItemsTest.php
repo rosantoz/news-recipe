c3f4503eeeaf6f585e5a089b9f21fcd7adea3f89
Rodrigo <email@rodrigodossantos.ws>
2014-02-22 14:06:20 +1100

    Create tests directory

---
+++ tests/app/src/Fridge/ItemsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tests\Fridge;
+
+
+class ItemsTest extends \PHPUnit_Framework_TestCase
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+} 
---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+error_reporting(-1);
+date_default_timezone_set('Australia/Sydney');
+
+require_once '../vendor/autoload.php';
