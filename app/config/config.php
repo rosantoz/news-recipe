0dd4cc47981293b319767a3806894b2f0e2be2cd
Rodrigo <email@rodrigodossantos.ws>
2014-02-22 14:00:23 +1100

    Add classess namespaces to composer.json and remove previous custom autoloader

--- app/config/config.php
+++ app/config/config.php
@@ -1,7 +1,2 @@
 <?php
-
-function __autoload($class)
-{
-    $class = "../app/src/" . str_replace('\\', '/', $class) . ".php";
-    require_once "$class";
-}
+date_default_timezone_set('Australia/Sydney');
